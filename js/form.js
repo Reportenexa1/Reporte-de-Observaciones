07661e71a9f4514980ca91f4e5f3ae63e5f59b5b
Reportenexa1 <77400285+Reportenexa1@users.noreply.github.com>
2021-01-14 20:19:11 -0500

    Update form.js

--- js/form.js
+++ js/form.js
@@ -2,31 +2,34 @@ document.querySelector("#submit").addEventListener("click", e => {
   e.preventDefault();
 
   //INGRESE UN NUMERO DE WHATSAPP VALIDO AQUI:
-  let telefono = "GgqKa3NsZY2CFtBrSYx6tK";
+  let telefono = "51933429595";
 
   let cliente = document.querySelector("#cliente").value;
   let fecha = document.querySelector("#fecha").value;
   let hora = document.querySelector("#hora").value;
   let empleado = document.querySelector("#empleado").value;
+  let compromiso = document.querySelector("#compromiso").value;
   let servicio = document.querySelector("#servicio").value;
   let resp = document.querySelector("#respuesta");
 
   resp.classList.remove("fail");
   resp.classList.remove("send");
 
-  let url = `https://chat.whatsapp.com/GgqKa3NsZY2CFtBrSYx6tK&text=  
+  let url = `https://api.whatsapp.com/send?phone=${telefono}&text=
 	*_REPORTE DE OBSERVACIONES_*%0A
 	*MANTENIMIENTO*%0A%0A
-	*OBSERVADOR*%0A
-	${cliente}%0A
-	*ACTIVIDAD*%0A
-	${empleado}%0A
-	*FECHA*%0A
-	${fecha}%0A
-	*DETALLE*%0A
-	${servicio}%0A
-	*COMPROMISO*%0A
-	${hora}`;
+  *OBSERVADOR*%0A
+  *FECHA*%0A
+  ${fecha}%0A
+  *HORA*%0A
+  ${hora}%0A
+  *ACTIVIDAD CRITICA - LUGAR*%0A
+  ${empleado}%0A
+	*DESCRIPCIÓN*%0A
+  ${servicio}%0A
+  *COMPROMISO*%0A
+  ${compromiso}%0A
+	`;
 
   if (cliente === "" || fecha === "" || hora === "") {
     resp.classList.add("fail");
@@ -39,3 +42,4 @@ document.querySelector("#submit").addEventListener("click", e => {
 
   window.open(url);
 });
+
